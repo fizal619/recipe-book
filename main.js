da722caac0132ff6dc50e0b3f7b49121a803a4d5
Fizal Sarif <fizal.sarif@gmail.com>
2024-07-17 23:37:44 -0400

    minor fixes

--- main.js
+++ main.js
@@ -86,16 +86,23 @@ async function deleteRecipe() {
   });
 }
 
-async function viewRecipe(filename, userId) {
-  const recipe = await getSingleRecipe(filename, userId);
-  const username = await getUserName(userId);
+async function viewRecipe(filename, recipeUserId) {
+  const recipe = await getSingleRecipe(filename, recipeUserId);
+  const username = await getUserName(recipeUserId);
 
   const converter = new showdown.Converter();
-  const recipeHTML = converter.makeHtml(recipe)
+  const recipeHTML = converter.makeHtml(recipe);
+
   document.getElementById('recipeBox').innerHTML = recipeHTML;
   document.getElementById('recipeUser').textContent = `${username}`;
 
   deleteRecipeFilename=filename;
+
+  if (recipeUserId === userId) {
+    document.getElementById('deleteRecipeBtn').classList.remove('is-hidden');
+  } else {
+    document.getElementById('deleteRecipeBtn').classList.add('is-hidden');
+  }
 
   goto("viewRecipeScreen");
 }
@@ -114,13 +121,16 @@ async function render() {
 
   if (userId) {
     const userRecipes = await getRecipes(userId);
-    console.log(userRecipes);
+
     recipeList.innerHTML = userRecipes.map(x => `
       <div onclick="viewRecipe('${x}', '${userId}')" class="box is-flex is-justify-content-space-between recipe-item">
         <p>${x}</p>
       </div>
     `).join("");
   }
+
+  renderFriendListScreen();
+
   goto(currentScreen);
 }
 
@@ -154,10 +164,20 @@ function createRecipe() {
         'user_secret': userSecret
       }
     })
-  }).then(x => x.json()).then(x => {
+  }).then(x => x.text()).then(x => {
     console.log(x);
+    Toastify({
+      text: "Recipe will be created shortly. Refresh to see changes in a minute.",
+      className: "success",
+      position: "center"
+    }).showToast();
   }).catch(x => {
     console.log(x);
+    Toastify({
+      text: "Failed to create recipe. Please try again later.",
+      className: "success",
+      position: "center"
+    }).showToast();
   });
   recipeName.value = '';
   recipeDescription.value = '';
@@ -165,16 +185,83 @@ function createRecipe() {
   goto('recipeListScreen');
 }
 
+function addFriend() {
+  const friendCode = prompt("Enter friend code:");
+  const friends = localStorage.getItem('friends') || "[]";
+  const friendsList = JSON.parse(friends);
+  const friendSet = new Set(friendsList);
+  if (!friendSet.has(friendCode)) {
+    friendsList.push(friendCode);
+    localStorage.setItem('friends', JSON.stringify(friendsList));
+    renderFriendListScreen();
+  }
+}
+
+async function getFriendNames(friendsList) {
+  const friendNames = {};
+  for (const friend of friendsList) {
+    const username = await getUserName(friend);
+    friendNames[friend] = username;
+  }
+  return friendNames;
+}
+
+async function getFriendRecipes(friendList) {
+  const friendRecipes = {};
+  for (const friend of friendList) {
+    const userRecipes = await getRecipes(friend);
+    friendRecipes[friend] = userRecipes;
+  }
+  return friendRecipes;
+}
+
+async function renderFriendListScreen() {
+  const friends = localStorage.getItem('friends') || "[]";
+  const friendsList = JSON.parse(friends);
+  const friendNames = await getFriendNames(friendsList);
+  document.getElementById('friendList').innerHTML = friendsList.map(x => `
+    <div class="box is-flex is-justify-content-space-between friend-item">
+      <p>${friendNames[x]}</p>
+    </div>
+  `).join("");
+
+  const friendRecipes = await getFriendRecipes(friendsList);
+  document.getElementById('friendRecipeList').innerHTML = friendsList.map(x => {
+    const recipes = friendRecipes[x];
+    return recipes.map(y => `
+      <div onclick="viewRecipe('${y}', '${x}')" class="box is-flex is-justify-content-space-between recipe-item">
+        <p>${y} by ${friendNames[x]}</p>
+      </div>
+    `).join("");
+  }).join("");
+
+}
+
 document.getElementById('createUserBtn').addEventListener('click', registerUser);
 document.getElementById('createRecipeBtn').addEventListener('click', () => {
   goto('createRecipeScreen');
 });
+document.getElementById('friendsBtn').addEventListener('click', () => {
+  goto('friendReceipeListScreen');
+});
 document.querySelectorAll('.cancelRecipeBtn').forEach(x => x.addEventListener('click', () => {
   goto('recipeListScreen');
 }));
+
 document.getElementById('deleteRecipeBtn').addEventListener('click', deleteRecipe);
 
 document.getElementById('submitRecipeBtn').addEventListener('click', createRecipe);
+
+document.getElementById("friendCode").addEventListener("click", () => {
+  navigator.clipboard.writeText(userId);
+  Toastify({
+    text: "Friend code copied to clipboard.",
+    className: "success",
+    position: "center"
+  }).showToast();
+})
+
+document.getElementById('addFriendButton').addEventListener('click',addFriend);
 
 render();
 
