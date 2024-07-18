fc346b349fcb2f823c64ff9c5022f64de8223f90
Fizal Sarif <fizal.sarif@gmail.com>
2024-07-17 22:48:54 -0400

    update create recipe

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const otherHalf = "uvFAKqNIoDczm6rhgfFVHNDWauBfXKHDC6Soj9mo205TWBZUXW5wkPgoGVS";
 
 let currentScreen = "recipeListScreen";
 let userId = "";
+let deleteRecipeFilename = "";
 
 function goto(pageName) {
   const screens = document.getElementsByClassName('page');
@@ -13,6 +14,7 @@ function goto(pageName) {
   }
   document.getElementById(pageName).classList.remove('is-hidden');
 }
+
 async function getRecipes(userId) {
   const response = await fetch(
     `https://raw.githubusercontent.com/fizal619/recipe-book/main/recipes/${userId}/index.list?token=${Date.now()}`,
@@ -23,6 +25,79 @@ async function getRecipes(userId) {
     return [];
   }
   return data.split("\n").filter(x => x !== "");
+}
+
+async function getSingleRecipe(filename, userId) {
+  const response = await fetch(
+    `https://raw.githubusercontent.com/fizal619/recipe-book/main/recipes/${userId}/${filename}?token=${Date.now()}`
+  );
+  const data = await response.text();
+  if (response.status === 404) {
+    return "Recipe was probably deleted, go back and try again.";
+  }
+  return data;
+}
+
+async function getUserName(userId) {
+  const response = await fetch(
+    `https://raw.githubusercontent.com/fizal619/recipe-book/main/users/${userId}/name?token=${Date.now()}`
+  );
+  const data = await response.text();
+  if (response.status === 404) {
+    return "";
+  }
+  return data;
+}
+
+async function deleteRecipe() {
+  const username = localStorage.getItem('username');
+  const userSecret = localStorage.getItem('userSecret');
+  const auth = `${prefix}${half}${otherHalf}`;
+  const url = `https://api.github.com/repos/fizal619/recipe-book/actions/workflows/delete_recipe.yaml/dispatches`;
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Authorization': `token ${auth}`,
+      'Accept': 'application/vnd.github.v3+json',
+    },
+    body: JSON.stringify({
+      'ref': 'main',
+      'inputs': {
+        'filename': deleteRecipeFilename,
+        'username': username,
+        'user_secret': userSecret
+      }
+    })
+  }).then(x => x.text()).then(x => {
+    Toastify({
+      text: "Recipe will be deleted shortly. Refresh to see changes in a minute.",
+      className: "success",
+      position: "center"
+    }).showToast();
+    currentScreen = "recipeListScreen";
+    render();
+  }).catch(x => {
+    console.log(x);
+    Toastify({
+      text: "Failed to delete recipe. Please try again later.",
+      className: "danger",
+      position: "center"
+    }).showToast();
+  });
+}
+
+async function viewRecipe(filename, userId) {
+  const recipe = await getSingleRecipe(filename, userId);
+  const username = await getUserName(userId);
+
+  const converter = new showdown.Converter();
+  const recipeHTML = converter.makeHtml(recipe)
+  document.getElementById('recipeBox').innerHTML = recipeHTML;
+  document.getElementById('recipeUser').textContent = `${username}`;
+
+  deleteRecipeFilename=filename;
+
+  goto("viewRecipeScreen");
 }
 
 async function render() {
@@ -41,7 +116,7 @@ async function render() {
     const userRecipes = await getRecipes(userId);
     console.log(userRecipes);
     recipeList.innerHTML = userRecipes.map(x => `
-      <div onclick="openRecipe('${x}')" class="box is-flex is-justify-content-space-between recipe-item">
+      <div onclick="viewRecipe('${x}', '${userId}')" class="box is-flex is-justify-content-space-between recipe-item">
         <p>${x}</p>
       </div>
     `).join("");
@@ -59,7 +134,6 @@ function registerUser() {
 
 function createRecipe() {
   const recipeName = document.getElementById('recipeName');
-  const recipeDescription = document.getElementById('recipeDescription');
   const recipeContent = document.getElementById('recipeContent');
   const username = localStorage.getItem('username');
   const userSecret = localStorage.getItem('userSecret');
@@ -75,7 +149,6 @@ function createRecipe() {
       'ref': 'main',
       'inputs': {
         'name': recipeName.value,
-        'description': recipeDescription.value,
         'steps': recipeContent.value,
         'username': username,
         'user_secret': userSecret
@@ -96,9 +169,11 @@ document.getElementById('createUserBtn').addEventListener('click', registerUser)
 document.getElementById('createRecipeBtn').addEventListener('click', () => {
   goto('createRecipeScreen');
 });
-document.getElementById('cancelRecipeBtn').addEventListener('click', () => {
+document.querySelectorAll('.cancelRecipeBtn').forEach(x => x.addEventListener('click', () => {
   goto('recipeListScreen');
-});
+}));
+document.getElementById('deleteRecipeBtn').addEventListener('click', deleteRecipe);
+
 document.getElementById('submitRecipeBtn').addEventListener('click', createRecipe);
 
 render();
